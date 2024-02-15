3b04a95f6d2e41359ed0e73ebf5e8432bd44c2d4
Saritha V <saroo.vasa@gmail.com>
2024-02-15 16:30:41 -0500

    More Character

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,4 +1,7 @@
 class Character:
     name: str = "Character"
     move_count: int = 0
-    current_position: tuple = (0,0)
+    current_position: tuple = (5,5)
+
+    def getStatus():
+        print  ("Inside Character getStatus")
