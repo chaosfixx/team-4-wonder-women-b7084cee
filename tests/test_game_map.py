8f9832381751388d518c5f8ff3e8655d01181f82
Anna Cecilia <annadraper08@gmail.com>
2024-02-15 15:17:48 -0500

    game map testing

---
+++ tests/test_game_map.py
@@ -0,0 +1,11 @@
+import unittest import TestCase
+from unittest.mock import MagicMock, create_autospec
+from map import GameMap
+
+class TestMap(unittest.TestCase):
+    def test_initialization(self):
+        testObj = map()
+        x_minimum_value = 0
+        x_maximum = 9
+        self.start_x > x_minimum_value
+        self.start_x < x_maximum 
