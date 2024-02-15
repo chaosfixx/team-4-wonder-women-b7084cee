06e8148cda7dbbbfe521ccd0ba582373ccd2662e
Anna Cecilia <annadraper08@gmail.com>
2024-02-15 17:01:49 -0500

    3 tests for gamemap passing

--- src/levelup/gamemap.py
+++ src/levelup/gamemap.py
@@ -1,3 +1,10 @@
 
 class GameMap:
     status: str = "Default"
+    value: int = None
+    x_minimum_value: int = 0
+    x_maximum_value: int = 9
+    y_minimum_value: int = 0
+    y_maximum_value: int = 9
+    minimum_value: int = 0
+    maximum_value: int = 9
--- tests/test_game_map.py
+++ tests/test_game_map.py
@@ -2,6 +2,55 @@ import unittest
 from levelup.gamemap import GameMap
 
 class TestMap(unittest.TestCase):
-    def test_initialization(self):
-        testObj = GameMap()
-        assert testObj.status != None
+    @classmethod
+    def setUpClass(cls):
+        x_minimum_value = 0
+        x_maximum_value = 9
+        y_minimum_value = 0
+        y_maximum_value = 9
+    
+    @classmethod
+    def tearDownClass(cls):
+        pass
+    
+    def test_x_initialization(self):
+        testobj = GameMap()
+        assert testobj.status != None
+
+        x_minimum_value = 0
+        x_maximum_value = 9
+        
+        self.assertEqual(
+            x_minimum_value,
+            testobj.minimum_value
+        )
+        self.assertEqual (
+            x_maximum_value,
+            testobj.maximum_value
+        )
+
+    def test_y_initialization(self):
+        testobj = GameMap()
+
+        y_minimum_value = 0
+        y_maximum_value = 9
+
+        self.assertEqual (
+            y_minimum_value,
+            testobj.minimum_value,
+        )
+        self.assertEqual(
+            y_maximum_value,
+            testobj.maximum_value
+        )
+
+    # def y_outofbounds (
+    #     if y_maximum_value >= 10 
+    #     if y_minimum_value < 0
+    #     return "Y is out of bounds"
+    # )
+    # def x_outofbounds (
+    #     if x_maximum_value >= 10
+    #     if x_minimum_value < 0
+    #     return "X is out of bounds"
+    # )
