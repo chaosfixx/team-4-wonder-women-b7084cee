40e669cde609cda07c37a45a0fab2b96cbc47888
Shubhaga24 <Shubha_ga@yahoo.com>
2024-02-15 17:50:11 -0500

    enter

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -3,7 +3,10 @@ from dataclasses import dataclass
 from enum import Enum
 
 class Position(int):
-    x_position: int = 4
-    y_position: int = 4
+    x_position: int = 5
+    y_position: int = 5
+
+def getCurrentPosition():
+    print("My Current Position: " + this.x_position + "," + this.y_position)
 
 
