85984ae5453976bc68d9e4fc2e14dd6081be21eb
Shubhaga24 <Shubha_ga@yahoo.com>
2024-02-15 16:39:06 -0500

    position class

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -5,3 +5,7 @@ from enum import Enum
 class Position(int):
     x_position: int = 4
     y_position: int = 4
+    minimum_positionx: int = 0
+    maximum_positionx: int = 9
+    minimum_positiony: int = 0
+    maximum_positiony: int = 9
--- tests/test_position.py
+++ tests/test_position.py
@@ -10,3 +10,33 @@ class TestPosition(TestCase):
         x_position = 4
         y_position = 4
         #assert testObj.status != None
+
+    def test_init(self):
+        testObj = Position()
+        expected_minimum_positionx = 0
+        expected_maximum_positionx = 9
+        expected_minimum_positiony = 0
+        expected_maximum_positiony = 9
+        #assert testObj.status != None
+        self.assertEqual(
+            expected_minimum_positionx,
+            testObj.minimum_positionx
+        )
+        self.assertEqual(
+            expected_minimum_positiony,
+            testObj.minimum_positiony
+        )
+        self.assertEqual(
+            expected_maximum_positionx,
+            testObj.maximum_positionx
+        )
+        self.assertEqual(
+            expected_maximum_positiony,
+            testObj.maximum_positiony
+        )
+        #self.assertEqual(
+        #    x_position = 4,
+        #    y_position = 4
+        #)
+
+       
